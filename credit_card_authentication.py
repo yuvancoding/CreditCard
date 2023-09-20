f69a14655b257faa31860938250c5ab4c684f40f
yuvancoding <112766393+yuvancoding@users.noreply.github.com>
2023-09-20 19:52:04 +0530

    Add files via upload

---
+++ credit_card_authentication.py
@@ -0,0 +1,29 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root=Tk()
+root.title("Credit Card Authentication")
+root.geometry("600x400")
+
+root.configure(background="gold")
+
+input_box = Entry()
+input_box.place(relx=0.5, rely=0.3, anchor = CENTER)
+
+img=ImageTk.PhotoImage(Image.open ("credit.jpg"))
+label = Label(root, image=img)
+
+
+def authentication():
+    try:
+        input_value = int(input_box.get())
+        messagebox.showinfo("Alert","Credit card accepted.")
+    except(ValueError):
+        messagebox.showinfo("Credit card not accepted.")
+
+
+btn = Button(root, text = "check credit card", command = authentication)
+btn.place(relx=0.5, rely=0.4, anchor = CENTER)
+label.place(relx=0.5, rely=0.7, anchor = CENTER)
+
+root.mainloop()
